7b757cd01e3193429b76922e35237af5803a692f
lucien <2172856294@qq.com>
2025-01-20 00:26:23 -0800

    add fv files for 3-staged aes-sbox

---
+++ experiments/dep_sni/run_g.sh
@@ -0,0 +1,3 @@
+./maskverif < mv/aes/fv/o1/aes_sbox_g.mv  >> experiments/dep_sni/o1.txt 2>&1
+./maskverif < mv/aes/fv/o2/aes_sbox_g.mv  >> experiments/dep_sni/o2.txt 2>&1
+./maskverif < mv/aes/fv/o3/aes_sbox_g.mv  >> experiments/dep_sni/o3.txt 2>&1
